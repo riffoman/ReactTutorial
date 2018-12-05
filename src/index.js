d7b408f3298f856515dc5790b446093984181982
sajinovics <srdjan.sajinovic@lanaco.com>
2018-12-05 14:45:33 +0100

    dodana shopping listz komponenta

--- src/index.js
+++ src/index.js
@@ -61,6 +61,21 @@ class Game extends React.Component {
 
 // ========================================
 
+class ShoppingList extends React.Component {
+    render() {
+      return (
+        <div className="shopping-list">
+          <h1>Shopping List for {this.props.name}</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </div>
+      );
+    }
+  }
+
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
